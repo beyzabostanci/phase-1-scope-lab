a92f552dd5fd1690d1b78b6ce73383c29be005cc
beyzabostanci <beyza_bostanc@hotmail.com>
2022-04-18 15:35:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+}// now it can use global variable
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+}// declaring global variable from İNSİDE a function-undefined
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = "vera"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "reha"
+}
